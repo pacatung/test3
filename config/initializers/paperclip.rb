a19ada6fbbdc5f77236490954105a5fbb4dc4d2e
BigBoss <pacatung@gmail.com>
2016-09-05 01:04:03 +0800

    s3 config

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = ':northcali1.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] ='/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
